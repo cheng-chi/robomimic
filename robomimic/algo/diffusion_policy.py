064fcd7a1e2c68127d7a9febbf2138137e224411
Cheng Chi <cheng.chi@columbia.edu>
2023-05-08 21:36:43 -0400

    put action range check back

--- robomimic/algo/diffusion_policy.py
+++ robomimic/algo/diffusion_policy.py
@@ -130,8 +130,8 @@ class DiffusionPolicyUNet(PolicyAlgo):
             actions = input_batch["actions"]
             in_range = (-1 <= actions) & (actions <= 1)
             all_in_range = torch.all(in_range).item()
-            # if not all_in_range:
-            #     raise ValueError('"actions" must be in range [-1,1] for Diffusion Policy! Check if hdf5_normalize_action is enabled.')
+            if not all_in_range:
+                raise ValueError('"actions" must be in range [-1,1] for Diffusion Policy! Check if hdf5_normalize_action is enabled.')
             self.action_check_done = True
         
         return TensorUtils.to_device(TensorUtils.to_float(input_batch), self.device)
